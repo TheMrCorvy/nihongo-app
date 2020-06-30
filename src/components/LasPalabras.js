62a34530e2b96b3c29ee700bb52d217c39d1f857
gonzalo salvador corvalan <mr.corvy@gmail.com>
2020-06-30 18:45:58 -0300

    s

---
+++ src/components/LasPalabras.js
@@ -0,0 +1,140 @@
+const LasPalabras = {
+	Elementos: [
+		{
+			titulo: "comida y bebida",
+			bgColor:
+				"linear-gradient(87deg, rgb(45, 206, 137) 0px, rgb(45, 206, 204) 100%)",
+			sombra: "rgba(45, 206, 137, 0.6) 0px 5px 25px 0px",
+			textColor: "text-white",
+			elementos: [
+				{
+					tituloElemento: "comida",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "bebida",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "frutas y verduras",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "dulces",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "cocina",
+					url: "ejemplo",
+				},
+			],
+		},
+		{
+			titulo: "viajes",
+			bgColor:
+				"linear-gradient(87deg, rgb(17, 205, 239) 0px, rgb(17, 113, 239) 100%)",
+			sombra: "rgba(44, 168, 255, 0.6) 0px 5px 25px 0px",
+			textColor: "text-white",
+			elementos: [
+				{
+					tituloElemento: "transporte",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "vocabulario de aeropuerto",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "vacaciones",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "lugares y ubicaciones",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "lugares de japón",
+					url: "ejemplo",
+				},
+			],
+		},
+		{
+			titulo: "ropa",
+			bgColor:
+				"linear-gradient(87deg, rgb(247, 165, 153) 0px, rgb(235, 172, 163) 100%)",
+			sombra: "rgba(247, 165, 153, 0.7) 0px 5px 25px 0px",
+			textColor: "text-white",
+			elementos: [
+				{
+					tituloElemento: "partes del cuerpo",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "ropa",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "vestirse",
+					url: "ejemplo",
+				},
+			],
+		},
+		{
+			titulo: "los adverbios",
+			bgColor: "linear-gradient(87deg,#172b4d 0,#1a174d 100%)",
+			sombra: "0px 5px 25px 0px rgba(26, 23, 77, 0.6)",
+			textColor: "text-white",
+			elementos: [
+				{
+					tituloElemento: "derivados de los adjetivos",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "adverbios verdaderos",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "la forma て como adverbio",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "de frecuencia",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "de tiempo",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "de modo",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "de lugar",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "de cantidad",
+					url: "ejemplo",
+				},
+				{
+					tituloElemento: "de duda",
+					url: "ejemplo",
+				},
+			],
+		},
+		{
+			titulo: "sin grupo",
+			textColor: "text-white",
+			elementos: [
+				{
+					tituloElemento: "los animales",
+					url: "ejemplo",
+					icono: "paw",
+				},
+			],
+		},
+	],
+};
+
+export default LasPalabras;
--- src/components/Transiciones/VocabularioPalabras.jsx
+++ src/components/Transiciones/VocabularioPalabras.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Card, CardBody, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 import Secciones from "components/Sections/Secciones";
+import LasPalabras from "components/LasPalabras";
 
 export default class VocabularioPalabras extends React.Component {
 	constructor(props) {
@@ -20,436 +21,115 @@ export default class VocabularioPalabras extends React.Component {
 			height: "0px",
 		});
 	}
+
+	renderizarPorCadaSeccion = (seccion) => {
+		if (seccion.titulo === "sin grupo") {
+			return seccion.elementos.map((elemento) => (
+				<Col md="6" key={elemento.tituloElemento}>
+					<Link
+						to={"/" + elemento.url}
+						onClick={() => {
+							this.props.scrollTop();
+						}}
+					>
+						<Card
+							style={{
+								borderRadius: "10px",
+								boxShadow:
+									"rgba(251, 99, 64, 0.6) 0px 5px 25px 0px",
+								background:
+									"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
+							}}
+						>
+							<CardBody className="text-white text-capitalize">
+								<span className="title">
+									{elemento.tituloElemento}
+								</span>
+								<i className="fas fa-chevron-right pl-3"></i>
+								<i
+									className={"fas fa-3x fa-" + elemento.icono}
+									style={{
+										position: "absolute",
+										top: 10,
+										right: 10,
+										opacity: 0.7,
+									}}
+								></i>
+							</CardBody>
+						</Card>
+					</Link>
+				</Col>
+			));
+		} else {
+			return (
+				<Col md="12" key={seccion.titulo}>
+					<Card
+						style={{
+							overflowY: "hidden",
+							height: "auto",
+							borderRadius: "15px",
+							background: seccion.bgColor,
+							boxShadow: seccion.sombra,
+						}}
+					>
+						<h4
+							className={
+								"text-center text-capitalize title mt-3 pt-0 " +
+								seccion.textColor
+							}
+						>
+							{seccion.titulo}
+						</h4>
+						<CardBody
+							className="d-flex justify-content-between px-0 col-lg-12"
+							style={{
+								overflowX: "scroll",
+								whiteSpace: "nowrap",
+								position: "relative",
+							}}
+						>
+							{seccion.elementos.map((elemento) => (
+								<Col md="auto" key={elemento.tituloElemento}>
+									<Link
+										to={"/" + elemento.url}
+										onClick={() => {
+											this.props.scrollTop();
+										}}
+									>
+										<Card
+											style={{
+												borderRadius: "10px",
+												background:
+													"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
+											}}
+											className="card-plain"
+										>
+											<CardBody className="text-white text-capitalize px-3">
+												<span className="title">
+													{elemento.tituloElemento}
+												</span>
+												<i className="fas fa-chevron-right pl-3"></i>
+											</CardBody>
+										</Card>
+									</Link>
+								</Col>
+							))}
+						</CardBody>
+					</Card>
+				</Col>
+			);
+		}
+	};
 	render() {
 		return (
 			<React.Fragment>
 				<h3 className="title text-center pt-2">
 					Vocabulario de Palabras Generales
 				</h3>
-				<Row className="justify-content-around px-3">
-					<Col
-						md="12"
-						className="px-0 mb-4"
-						style={{
-							overflowY: "hidden",
-							height: this.state.height,
-							borderRadius: "15px",
-							background:
-								"linear-gradient(87deg, rgb(45, 206, 137) 0px, rgb(45, 206, 204) 100%)",
-							boxShadow:
-								"rgba(45, 206, 137, 0.6) 0px 5px 25px 0px",
-						}}
-					>
-						<h4 className="text-white text-center title mt-3 pt-0">
-							Comida y Bebida
-						</h4>
-						<div
-							className="d-flex justify-content-between px-0 col-lg-12"
-							style={{
-								overflowX: "scroll",
-								whiteSpace: "nowrap",
-								position: "relative",
-							}}
-						>
-							<Col md="auto">
-								<Link
-									to="/verbos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												comida
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/adjetivos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												bebida
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/sustantivos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												frutas y verduras
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/sustantivos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												dulces
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/sustantivos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												cocina
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-						</div>
-					</Col>
-					<Col
-						md="12"
-						className="px-0 mb-4"
-						style={{
-							overflowY: "hidden",
-							height: this.state.height,
-							borderRadius: "15px",
-							background:
-								"linear-gradient(87deg, rgb(17, 205, 239) 0px, rgb(17, 113, 239) 100%)",
-							boxShadow:
-								"rgba(44, 168, 255, 0.6) 0px 5px 25px 0px",
-						}}
-					>
-						<h4 className="text-white text-center title mt-3 pt-0">
-							Viajes
-						</h4>
-						<div
-							className="d-flex justify-content-between px-0 col-lg-12"
-							style={{
-								overflowX: "scroll",
-								whiteSpace: "nowrap",
-								position: "relative",
-							}}
-						>
-							<Col md="auto">
-								<Link
-									to="/verbos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												transporte
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/verbos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												{/* visa pasaporte avion aeropuerto, etc */}
-												vocabulario de aeropuerto
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/adjetivos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												vacaciones
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/sustantivos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												lugares y ubicaciones
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/sustantivos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												lugares de japón
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-						</div>
-					</Col>
-					<Col
-						md="12"
-						className="px-0 mb-4"
-						style={{
-							overflowY: "hidden",
-							height: this.state.height,
-							borderRadius: "15px",
-							background:
-								"linear-gradient(87deg, rgb(247, 165, 153) 0px, rgb(235, 172, 163) 100%)",
-							boxShadow:
-								"rgba(247, 165, 153, 0.7) 0px 5px 25px 0px",
-						}}
-					>
-						<h4 className="text-white text-center title mt-3 pt-0">
-							Ropa
-						</h4>
-						<div
-							className="d-flex justify-content-between px-0 col-lg-12"
-							style={{
-								overflowX: "scroll",
-								whiteSpace: "nowrap",
-								position: "relative",
-							}}
-						>
-							<Col md="auto">
-								<Link
-									to="/verbos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												partes del cuerpo
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/verbos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">ropa</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-							<Col md="auto">
-								<Link
-									to="/verbos"
-									onClick={() => {
-										this.props.scrollTop();
-									}}
-								>
-									<Card
-										style={{
-											borderRadius: "10px",
-											background:
-												"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-										}}
-										className="card-plain"
-									>
-										<CardBody className="text-white text-capitalize px-3">
-											<span className="title">
-												vestirse
-											</span>
-											<i className="fas fa-chevron-right pl-3"></i>
-										</CardBody>
-									</Card>
-								</Link>
-							</Col>
-						</div>
-					</Col>
-					<Col md="6" className="px-0">
-						<Link
-							to="/verbos"
-							onClick={() => {
-								this.props.scrollTop();
-							}}
-						>
-							<Card
-								style={{
-									borderRadius: "10px",
-									boxShadow:
-										"rgba(251, 99, 64, 0.6) 0px 5px 25px 0px",
-									background:
-										"linear-gradient(87deg, rgb(251, 99, 64) 0px, rgb(251, 177, 64) 100%)",
-								}}
-							>
-								<CardBody className="text-white text-capitalize px-3">
-									<span className="title">los animales</span>
-									<i className="fas fa-chevron-right pl-3"></i>
-									<i
-										className="fas fa-3x fa-paw"
-										style={{
-											position: "absolute",
-											top: 10,
-											right: 10,
-											opacity: 0.7,
-										}}
-									></i>
-								</CardBody>
-							</Card>
-						</Link>
-					</Col>
-				</Row>
 				<Row className="justify-content-around">
+					{LasPalabras.Elementos.map((seccion) =>
+						this.renderizarPorCadaSeccion(seccion)
+					)}
 					<h3 className="title text-center col-lg-12 px-0">
 						Todas las Palabras
 					</h3>
