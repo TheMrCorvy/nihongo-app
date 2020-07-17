f96ffcb99f1844e49aa3812e8ec8ef18b14926be
gonzalo salvador corvalan <mr.corvy@gmail.com>
2020-07-17 19:50:52 -0300

    s

--- src/components/LosKanjis.js
+++ src/components/LosKanjis.js
@@ -3,12 +3,14 @@ import Parte2 from "components/Jsons/Kanjis/Parte2.json";
 import Parte3 from "components/Jsons/Kanjis/Parte3.json";
 import Parte4 from "components/Jsons/Kanjis/Parte4.json";
 import Parte5 from "components/Jsons/Kanjis/Parte5.json";
+import Parte6 from "components/Jsons/Kanjis/Parte6.json";
 
 const LosKanjis = Parte1.Parte1.concat(
 	Parte2.Parte2,
 	Parte3.Parte3,
 	Parte4.Parte4,
-	Parte5.Parte5
+	Parte5.Parte5,
+	Parte6.Parte6
 );
 
 export default LosKanjis;
