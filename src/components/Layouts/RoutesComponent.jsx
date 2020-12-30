f3bba8e089a57093da9d5a2dbbe7c3b02beb3946
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2020-12-30 14:03:43 -0300

    changed the routes lets see if it works

--- src/components/Layouts/RoutesComponent.jsx
+++ src/components/Layouts/RoutesComponent.jsx
@@ -1,19 +1,15 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
-import routes from "components/routes";
+import routes from 'components/routes';
 
 export default function RoutesComponent() {
-	return (
-		<Switch>
-			{routes.map((route) => (
-				<Route
-					exact
-					path={route.path}
-					component={route.component}
-					key={route.nameKey}
-				/>
-			))}
-		</Switch>
-	);
+    return (
+        <Switch>
+            {routes.map((route) => (
+                <Route path={route.path} component={route.component} key={route.nameKey} />
+            ))}
+            {/* <Redirect from="/nihongo-app" to="/" /> */}
+        </Switch>
+    );
 }
