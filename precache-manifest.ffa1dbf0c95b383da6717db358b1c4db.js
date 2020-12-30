2bcab3e0ff39cfb8a7f4154dfe3bf4dcf86d54ce
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2020-12-30 13:59:31 -0300

    Updates

---
+++ precache-manifest.ffa1dbf0c95b383da6717db358b1c4db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9957b55a65f04634e3318e2256f0be6",
+    "url": "TheMrCorvy.github.io/nihongo-app/index.html"
+  },
+  {
+    "revision": "135b5c4d4e547ca29788",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/css/main.5c4eaefb.chunk.css"
+  },
+  {
+    "revision": "45982e7c04c7482222e5",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/js/2.6fc280da.chunk.js"
+  },
+  {
+    "revision": "135b5c4d4e547ca29788",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/js/main.97310fe4.chunk.js"
+  },
+  {
+    "revision": "cee2c448916bda133a3f",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/js/runtime~main.b1e91a56.js"
+  },
+  {
+    "revision": "0e796a31e4e823d33384b8bf45620e28",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/media/bg-image.0e796a31.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "TheMrCorvy.github.io/nihongo-app/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "TheMrCorvy.github.io/nihongo-app/precache-manifest.ffa1dbf0c95b383da6717db358b1c4db.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("TheMrCorvy.github.io/nihongo-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
